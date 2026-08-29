48b6cf2eb4ba8ccf38c27c8060abff919d50d921
Bhavnoor Singh <ytzindabad08@gmail.com>
2026-08-30 03:53:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -38,16 +38,51 @@ document.addEventListener("mousemove",(event) =>{
     const btnX = rect.left + rect.width/2;
     const btnY = rect.top + rect.height/2;
     const distance = Math.sqrt((event.clientX-btnX)**2 + (event.clientY-btnY)**2)
-    if (distance<38){
+    if (distance<37){
         bhagooo();
     }
 });
 function Iloose() {
-const btn = document.getElementById("no");
-if (btn.textContent === "Noo"){
-    btn.textContent = "Still Noo..😭";
+    const btn = document.getElementById("no");
+    if (btn.textContent === "Noo"){
+        btn.textContent = "Still Noo..😭";
+    }
+    else {
+        document.getElementById("popup").style.display = "flex";
+    }
 }
-else {
-    alert("Are you really sure? ");
+function closePopup() {
+        document.getElementById("popup").style.display = "none";
+        partyTime();
 }
+function newPopup() {
+        document.getElementById("popup").style.display = "none";
+        document.getElementById("popup2").style.display = "flex";
+}
+function closePopup2() {
+        document.getElementById("popup2").style.display = "none";
+        partyTime();
+}
+function partyTime() {
+    const confetti = document.getElementById("party");
+    for (let i = 0; i < 40; i++) {
+
+        const confetti = document.createElement("div");
+
+        confetti.classList.add("confetti");
+
+        confetti.textContent = "🎉";
+
+        confetti.style.left = Math.random() * 100 + "%";
+
+        confetti.style. animationDuration =(1.5 + Math.random() * 1.5) + "s";
+
+        confetti.style. animationDelay =Math.random() * 0.5 + "s";
+
+        party.appendChild(confetti);
+
+        setTimeout(()=> {
+        confetti.remove();
+        }, 3500);
+    }
 }
